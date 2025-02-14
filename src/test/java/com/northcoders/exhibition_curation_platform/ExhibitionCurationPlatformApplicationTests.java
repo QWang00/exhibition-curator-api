5d5c217cfce1eee14b003c5f794601ebee8b5134
qiuqiu <qiujuanwang87@gmail.com>
2025-02-14 12:09:52 +0000

    Initial Commit

---
+++ src/test/java/com/northcoders/exhibition_curation_platform/ExhibitionCurationPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.northcoders.exhibition_curation_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExhibitionCurationPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
