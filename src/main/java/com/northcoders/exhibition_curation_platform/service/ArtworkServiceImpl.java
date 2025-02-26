cef1f6a5efb23a1b1a736820f17a0bba714315a2
qiuqiu <qiujuanwang87@gmail.com>
2025-02-26 21:30:25 +0000

    feat: implement getArtworks in ArtworkServiceImpl class

---
+++ src/main/java/com/northcoders/exhibition_curation_platform/service/ArtworkServiceImpl.java
@@ -0,0 +1,36 @@
+package com.northcoders.exhibition_curation_platform.service;
+
+import com.northcoders.exhibition_curation_platform.model.Artwork;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.Collections;
+import java.util.List;
+
+@Service
+public class ArtworkServiceImpl implements ArtworkService{
+
+    private final HarvardApiClient harvardApiClient;
+    private final ClevelandApiClient clevelandApiClient;
+
+    @Autowired
+    public ArtworkServiceImpl(HarvardApiClient harvardApiClient, ClevelandApiClient clevelandApiClient) {
+        this.harvardApiClient = harvardApiClient;
+        this.clevelandApiClient = clevelandApiClient;
+    }
+
+    @Override
+    public List<Artwork> getArtworks(String keyword, String artist, String museum, int page) {
+        if ("Harvard".equals(museum)) {
+            return harvardApiClient.fetchArtworkList(keyword, artist, 10, page);
+        } else if ("Cleveland".equals(museum)) {
+            return clevelandApiClient.fetchArtworkList(keyword, 10, page * 10, artist);
+        }
+        return Collections.emptyList();
+    }
+
+    @Override
+    public Artwork getArtworkDetails(Artwork existingArtwork) {
+
+        return null;
+    }
+}
