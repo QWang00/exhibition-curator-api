d8a673c852b4ae0fb19e947ee735be0f9469e899
qiuqiu <qiujuanwang87@gmail.com>
2025-02-26 21:29:07 +0000

    feat: create ArtworkService interface with getArtworks & getArtworkDetails

---
+++ src/main/java/com/northcoders/exhibition_curation_platform/service/ArtworkService.java
@@ -0,0 +1,11 @@
+package com.northcoders.exhibition_curation_platform.service;
+
+import com.northcoders.exhibition_curation_platform.model.Artwork;
+
+import java.util.List;
+
+public interface ArtworkService {
+    List<Artwork> getArtworks (String keyword, String artist, String museum, int page);
+
+    Artwork getArtworkDetails (Artwork existingArtwork);
+}
