1d7eca576590698ee2863d07a9355b8a136e55c3
qiuqiu <qiujuanwang87@gmail.com>
2025-02-27 17:55:54 +0000

    fix: correct page parameter to ensure it starts from 1 not 0 in getArtworks of ArtworkServiceImpl

--- src/main/java/com/northcoders/exhibition_curation_platform/service/ArtworkServiceImpl.java
+++ src/main/java/com/northcoders/exhibition_curation_platform/service/ArtworkServiceImpl.java
@@ -19,11 +19,11 @@ public class ArtworkServiceImpl implements ArtworkService{
     }
 
     @Override
-    public List<Artwork> getArtworks(String keyword, String artist, String museum, int page) {
+    public List<Artwork> getArtworks(String keyword, String artist, String museum, int page) { // page starting from 1
         if ("Harvard Art Museum".equals(museum)) {
             return harvardApiClient.fetchArtworkList(keyword, artist, 10, page);
         } else if ("The Cleveland Museum of Art".equals(museum)) {
-            return clevelandApiClient.fetchArtworkList(keyword, 10, page * 10, artist);
+            return clevelandApiClient.fetchArtworkList(keyword, 10, (page-1) * 10, artist);
         }
         return Collections.emptyList();
     }
