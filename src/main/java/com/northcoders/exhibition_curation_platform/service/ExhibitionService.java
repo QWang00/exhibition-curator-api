79367a89dfb6421ed275c8d60b6cd83abac05eab
qiuqiu <qiujuanwang87@gmail.com>
2025-03-02 17:48:53 +0000

    feat: create ExhibitionService & methods

---
+++ src/main/java/com/northcoders/exhibition_curation_platform/service/ExhibitionService.java
@@ -0,0 +1,20 @@
+package com.northcoders.exhibition_curation_platform.service;
+
+import com.northcoders.exhibition_curation_platform.model.Exhibition;
+import java.util.List;
+
+public interface ExhibitionService {
+
+    List<Exhibition> getAllExhibitions ();
+
+    Exhibition getExhibitionById (Long id);
+
+    Exhibition createExhibition(String name);
+
+    Exhibition updateExhibitionName(Long exhibitionId, String newName);
+
+    Exhibition addArtworkToExhibition(Long exhibitionId, Integer sourceId, String museum);
+
+    Exhibition removeArtworkFromExhibition(Long exhibitionId, Long artworkId);
+
+}
