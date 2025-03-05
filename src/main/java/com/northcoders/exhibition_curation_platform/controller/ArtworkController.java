d4e619d8ca18041ffd340f7e6d27a7437bf3c869
qiuqiu <qiujuanwang87@gmail.com>
2025-03-05 14:53:48 +0000

    feat: add summary swagger descriptions for endpoints in ArtworkController

--- src/main/java/com/northcoders/exhibition_curation_platform/controller/ArtworkController.java
+++ src/main/java/com/northcoders/exhibition_curation_platform/controller/ArtworkController.java
@@ -24,7 +24,7 @@ public class ArtworkController {
     @Autowired
     ArtworkService artworkService;
 
-    @Operation(description = "Retrieve a list of artworks based on keywords (cached)")
+    @Operation(summary = "Get all artworks by keyword, artist, and museum", description = "Retrieve a list of artworks based on keywords (cached)")
     @GetMapping("/search-results/{museum}")
     @Cacheable
     public ResponseEntity<Map<String, Object>> getArtworks(
@@ -59,7 +59,7 @@ public class ArtworkController {
         return ResponseEntity.ok(response);
     }
 
-    @Operation(description = "Retrieve an artwork by its ID (cached)")
+    @Operation(summary = "Get artwork details using the artwork ID from its source museum", description = "Retrieve an artwork by its ID (cached)")
     @GetMapping("/{museum}/artwork/{sourceId}")
     @Cacheable(key = "#sourceId")
     public ResponseEntity<Artwork> getArtworkDetails(
